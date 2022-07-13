92a66f1c2153cc9cc3779eee6a6eff79a120b9ef
SANKET <32214164+sanket8088@users.noreply.github.com>
2022-07-13 23:39:04 +0530

    adding dropdown to function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello there people</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1>Hello there people</h1>, document.getElementById("root"))
+function DropDownList(){
+    return(<ul>
+        <li>Invoker</li>
+        <li>Dark Seer</li>
+        <li>Ember Spirit</li>
+        <li>Earth Spirit</li>
+    </ul>)
+}
+ReactDOM.render(<div><DropDownList /></div>, document.getElementById("root"))
